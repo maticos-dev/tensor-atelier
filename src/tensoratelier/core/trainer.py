da92002ce3902927af57f8fcfa3fa9ac8de3184e
Mateo del Rio <mdelriolanse@gmail.com>
2025-07-27 16:07:55 -0400

    Updated trainer to employ connectors and make corresponding calls to strategy and accelerator connectors, respectively/

--- src/tensoratelier/core/trainer.py
+++ src/tensoratelier/core/trainer.py
@@ -1,21 +1,33 @@
 from typing import Union
 
 import torch
+from tensorateier.strategies import Strategy
 
 from tensoratelier.core import AtelierModule
+from tensoratelier.core.connectors import AcceleratorHandler
 from tensoratelier.utils import _wrap_args
 
 
 class AtelierTrainer:
     @_wrap_args
     def __init__(
-        self, *, max_epochs: int = 10, device: Union[str, torch.device]
+        self,
+        *,
+        max_epochs: int = 10,
+        accelerator: Union[str, torch.device],
+        strategy: Union[str, Strategy],
     ) -> None:
         self.max_epochs = max_epochs
-        self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
+        self._accelerator_handler = AcceleratorHandler(accelerator, strategy)
 
     def fit(self, model: AtelierModule, train_loader, validation_loader) -> None:
-        model.to(self.device)
+        # NB: some kind of type checking for train_loader, validation_loader, and model.
+        # here error out the test script to see how these calls are made.
+
+        self._accelerator_handler.move_model(model)
+        self._accelerator_handler.move_dataloader(train_loader)
+        self._acceleration_handler.move_dataloader(validation_loader)
+
         optimizer = model.configure_optimizers()
 
         for epoch in range(self.max_epochs):
@@ -32,3 +44,7 @@ class AtelierTrainer:
                 for batch in validation_loader:
                     batch = [x.to(self.device) for x in batch]
                     model.validation_step(batch)
+
+    @property
+    def strategy(self) -> Strategy:
+        return self._accelerator_handler.strategy
