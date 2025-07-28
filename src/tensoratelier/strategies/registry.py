e67774da7c5a8da4d85165a33687d4df699c97e6
Mateo <mdelriolanse@gmail.com>
2025-07-27 21:39:25 -0400

    Committed protoype for the strategies registry.

--- src/tensoratelier/strategies/registry.py
+++ src/tensoratelier/strategies/registry.py
@@ -1,3 +1,8 @@
+from typing import Type, Union
+
+from tensoratelier.strategies import BaseStrategy
+
+
 class _StrategyRegistry:
     def __init__(self):
         self._registry = {}
@@ -7,7 +12,24 @@ class _StrategyRegistry:
         this method will only be called
         at the definition of each class
         """
-        pass
+
+        def wrapper(cls: Type[BaseStrategy]):
+            if strategy_type.lower() in self._registry:
+                raise ValueError("Strategy 'name' already registered")
+
+                self._registry[strategy_type.lower()] = cls
+
+        return wrapper
+
+    def _get(self, strategy_type: Union[str, BaseStrategy]) -> BaseStrategy:
+        key = strategy_type.lower()
+        # NB: Need to develop the basestrategy class
+
+        try:
+            return self._registry[key]()
+
+        except KeyError:
+            raise ValueError(f"Unsupported strategy '{key}'.")
 
 
 STRATEGY_REGISTRY = _StrategyRegistry()
