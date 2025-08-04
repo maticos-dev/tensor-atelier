5267a73ab1d96dcef6958042dd4beff29574808e
Mateo <mdelriolanse@gmail.com>
2025-08-04 05:56:35 -0400

    Fit method wrapped with automatic accelerator connections. Dataloader and model are now automatically moved over to appropriate accelerator.

--- src/tensoratelier/core/trainer.py
+++ src/tensoratelier/core/trainer.py
@@ -1,11 +1,11 @@
 from typing import Union
 
 import torch
-from tensorateier.strategies import Strategy
 
 from tensoratelier.core import AtelierModule
 from tensoratelier.core.connectors import AcceleratorHandler
-from tensoratelier.utils import _wrap_args
+from tensoratelier.core.connectors.utils import auto_move_dataloader, auto_move_model
+from tensoratelier.utils.parsing import _wrap_args
 
 
 class AtelierTrainer:
@@ -15,29 +15,25 @@ class AtelierTrainer:
         *,
         max_epochs: int = 10,
         accelerator: Union[str, torch.device],
-        strategy: Union[str, Strategy],
     ) -> None:
         self.max_epochs = max_epochs
-        self._accelerator_handler = AcceleratorHandler(accelerator, strategy)
+        self._accelerator_handler = AcceleratorHandler(accelerator)
 
+    @auto_move_dataloader
+    @auto_move_model
     def fit(self, model: AtelierModule, train_loader, validation_loader) -> None:
         # NB: some kind of type checking for train_loader, validation_loader, and model.
         # here error out the test script to see how these calls are made.
 
-        self._accelerator_handler.move_model(model)
-        self._accelerator_handler.move_dataloader(train_loader)
-        self._acceleration_handler.move_dataloader(validation_loader)
-
         optimizer = model.configure_optimizers()
+        self.validate_optimizer(optimizer)
 
         for epoch in range(self.max_epochs):
             model.train()
             for batch in train_loader:
-                batch = [x.to(self.device) for x in batch]
-                loss = model.training_step(batch)
-                loss.backward()
-                optimizer.step()
-                optimizer.zero_grad()
+                loss = model.train_step(batch)
+
+                self.backprop_and_reset_grads(loss, optimizer)
 
             model.eval()
             with torch.no_grad():
@@ -45,6 +41,21 @@ class AtelierTrainer:
                     batch = [x.to(self.device) for x in batch]
                     model.validation_step(batch)
 
-    @property
-    def strategy(self) -> Strategy:
-        return self._accelerator_handler.strategy
+    def backprop_and_reset_grads(self, loss, optimizer):
+        """
+        Loss returned from the model.train_step is a number, not the loss
+        class inself.
+        """
+
+        # if not isinstance(optimizer, torch.optim.Optimizer):
+        #     raise TypeError(
+        #         f"Expected type 'torch.optim.optimizer' but got {optimizer.__class__.__name__}"
+        #     )
+
+        if not isinstance(loss, torch.nn.Module):
+            raise TypeError(
+                f"Expected type 'torch.nn.Module' but got {loss.__class__.__name__}"
+            )
+            optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
