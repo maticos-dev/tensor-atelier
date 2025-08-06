34fedc8515735b47137eacc131928641fd68b5ef
Mateo <mdelriolanse@gmail.com>
2025-08-06 11:03:34 -0400

    New utils folder within loops to better compartmentalize menial, standalone functions during training. Calls to these functions will be made by the training_epoch_loops and fit_loop loops.

---
+++ src/tensoratelier/loops/__init__.py
@@ -0,0 +1,4 @@
+from .fit_loop import _FitLoop
+from .progress import _BatchProgress, _EpochProgress
+from .training_epoch_loop import _TrainingEpochLoop
+from .utils import _is_max_steps_reached
---
+++ src/tensoratelier/loops/utils.py
@@ -0,0 +1,2 @@
+def _is_max_steps_reached(current: int, maximum: int = 0) -> bool:
+    return current >= maximum
